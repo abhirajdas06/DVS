984cf7a82535f6da45805241e532bd5268346a2c
abhirajdas06 <abhirajdas57@gmail.com>
2022-07-15 18:07:51 +0530

    pages update

--- home/views.py
+++ home/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    if request.method == 'PSOT':
+        print ('hello')
     # return HttpResponse("Hello Home")
     return render(request, "home/index.html")
 
